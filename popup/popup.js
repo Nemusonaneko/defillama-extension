bdf97ae84dfe5297a876730b215b5755026193c4
nemusona <nemusonaneko@protonmail.com>
2021-12-08 14:25:06 -0600

    new api, data changes, and uhhh other stuff idk

--- popup/popup.js
+++ popup/popup.js
@@ -4,36 +4,25 @@ async function getCurrentTabUrl() {
   return tab[0].url;
 }
 
-async function returnTotalTvl(protocolInfo) {
-  let tvl = protocolInfo.tvl[protocolInfo.tvl.length - 1].totalLiquidityUSD;
-  return `$${Math.round(tvl).toLocaleString()}`;
+async function getTotalTvl(protocol) {
+  return `$${Math.round(protocol.tvl).toLocaleString()}`;
 }
 
-async function returnChange(protocolInfo, days) {
-  const currentTvl = Number(protocolInfo.tvl[protocolInfo.tvl.length - 1].totalLiquidityUSD);
-  const historicTvl = Number(protocolInfo.tvl[protocolInfo.tvl.length - (Number(days) + 1)].totalLiquidityUSD);
-  let change = Math.round(currentTvl - historicTvl);
-  let percentChange = Number((((currentTvl - historicTvl) / historicTvl) * 100).toFixed(2));
+async function historicalChange(protocol, period) {
+  let percentChange;
+  if (period === "hour") {
+    percentChange = protocol.change_1h;
+  } else if (period === "day") {
+    percentChange = protocol.change_1d;
+  } else if (period === "week") {
+    percentChange = protocol.change_7d;
+  }
+
   if (percentChange > 0) {
-    return `<span style='color:#5ec809'> +${await padPercentage(percentChange)}% $${change} </span>`;
+    return `<span style='color:#5ec809'> +${percentChange.toFixed(2)}% </span>`;
+  } else if (percentChange < 0) {
+    return `<span style='color:#f43e1c'> ${percentChange.toFixed(2)}% </span>`;
   }
-  else if (percentChange < 0) {
-    change = (change * -1).toLocaleString();
-    percentChange = percentChange * -1;
-    return `<span style='color:#f43e1c'> -${await padPercentage(percentChange)}% -$${change} </span>`;
-  }
-  else {
-    return `${formattedPercent}%`;
-  }
-}
-
-
-// I'm literally doing this cause I like symmetry
-async function padPercentage(percentChange) {
-  const split = percentChange.toString().split(".");
-  let left = split[0];
-  let right = split[1];
-  return `${left.padStart(2, "0")}.${right.padEnd(2,"0")}`;
 }
 
 async function getData() {
@@ -41,18 +30,32 @@ async function getData() {
   const data = await response.json();
   let hostname = new URL(await getCurrentTabUrl()).hostname;
   hostname = hostname.split(".");
-  const domain = hostname[hostname.length - 2] + "." + hostname[hostname.length - 1];
+  const domain =
+    hostname[hostname.length - 2] + "." + hostname[hostname.length - 1];
   if (data[domain] === undefined) return;
-  const protocolrequest = await fetch(`https://api.llama.fi/protocol/${data[domain]}`);
-  const protocolInfo = await protocolrequest.json();
-  document.getElementById("protocol_name").innerHTML = protocolInfo.name;
-  document.getElementById("site_link").href = `https://defillama.com/protocol/${data[domain]}`;
-  document.getElementById("total_tvl").innerHTML = await returnTotalTvl(protocolInfo);
-  document.getElementById("change_day").innerHTML = await returnChange(protocolInfo, 1);
-  document.getElementById("change_week").innerHTML = await returnChange(protocolInfo, 7);
-  document.getElementById("change_month").innerHTML = await returnChange(protocolInfo, 28);
+  const apiRequest = await fetch(`https://api.llama.fi/protocols`);
+  const apiResponse = await apiRequest.json();
+  let protocol;
+  apiResponse.forEach((p) => {
+    if (p.name.toLowerCase() === data[domain]) {
+      protocol = p;
+    }
+  });
+  document.getElementById("protocol_name").innerHTML = protocol.name;
+  document.getElementById("total_tvl").innerHTML = await getTotalTvl(protocol);
+  document.getElementById("change_hour").innerHTML = await historicalChange(
+    protocol,
+    "hour"
+  );
+  document.getElementById("change_day").innerHTML = await historicalChange(
+    protocol,
+    "day"
+  );
+  document.getElementById("change_week").innerHTML = await historicalChange(
+    protocol,
+    "week"
+  );
+  document.getElementById("site_link").href = `https://defillama.com/protocol/${data[domain].replace(" ", "-")}`;
 }
-
-
 
 getData();
